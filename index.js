f65b5d06841aef3f1249a080225ffdf652ec3bfd
FatihOztank <fatihoztank1997@gmail.com>
2023-01-15 20:53:07 +0300

    server is refactored according to new masa client

--- index.js
+++ index.js
@@ -7,37 +7,39 @@ let connections = []
 
 io.on("connect", (socket) => {
     connections.push(socket);
-    console.log(socket.id ,"has connected");
+    console.log(socket.id, "has connected");
 
     socket.on("mousedown", (data) => {
         let id = socket.id;
-        let x = data.x;
-        let y = data.y;
-        let selector_string = data.selector_string;
+        let selectorString = data.selectorString;
+        let iframeIndex = data.iframeIndex;
         socket.broadcast.emit("eventrecord", {
-            id, x, y, selector_string
+            id, selectorString, iframeIndex
         })
     });
 
     socket.on("mousemove", (data) => {
-        const x  = data.x;
+        const x = data.x;
         const y = data.y;
         const id = socket.id;
+        const iframeIndex = data.iframeIndex;
         socket.broadcast.emit("locationrecord", {
-            id,x,y
+            id, x, y, iframeIndex
         })
     })
 
-    socket.on("addedmutationrecord", (record) =>{
-        const target = record.mutationtarget;
-        const elemHTML = record.addedElemHTML;
+    socket.on("addedmutationrecord", (record) => {
+        const mutationTarget = record.mutationTarget;
+        const addedElemHTML = record.addedElemHTML;
+        const iframeIndex = record.iframeIndex;
         socket.broadcast.emit("addedmutation", {
-            target, elemHTML
+            mutationTarget, addedElemHTML, iframeIndex
         });
     });
 
-    socket.on("removedmutationrecord", () =>{
-        socket.broadcast.emit("deletemutations", {});
+    socket.on("removedmutationrecord", (record) => {
+        const iframeIndex = record.iframeIndex;
+        socket.broadcast.emit("deletemutations", { iframeIndex });
     });
 
     socket.on("modifiedAttributeRecord", (record) => {
@@ -45,19 +47,24 @@ io.on("connect", (socket) => {
         const mutationTarget = record.mutationTarget;
         const mutationValue = record.mutationValue;
         const mutatedElemHTML = record.mutatedElemHTML;
+        const iframeIndex = record.iframeIndex;
         socket.broadcast.emit("attributeModify", {
-            changedAttribute, mutationTarget, mutationValue, mutatedElemHTML
+            changedAttribute, mutationTarget, mutationValue, mutatedElemHTML, iframeIndex
         });
     })
 
     socket.on("disconnect", (reason) => {
         connections = connections.filter(con => con.id !== socket.id);
-        console.log(socket.id ,"has disconnected");
+        console.log(socket.id, "has disconnected");
 
     });
+
+    socket.on("opensecondiframe", () => {
+        socket.broadcast.emit("secondiframeopened", {});
+    })
 });
 
 let PORT = process.env.PORT || 8080;
-httpServer.listen(PORT,()=>{
+httpServer.listen(PORT, () => {
     console.log(`server started listening port ${PORT}`)
 });
