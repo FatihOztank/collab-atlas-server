f6de1fb1e771c64322b12ae467f983a88e757053
FatihOztank <fatihoztank1997@gmail.com>
2023-08-14 23:47:35 +0300

    server updates for the new atlas client

--- index.js
+++ index.js
@@ -8,78 +8,45 @@ let connections = []
 io.on("connect", (socket) => {
     connections.push(socket);
     console.log(socket.id, "has connected");
-
+    //checked
     socket.on("mousedown", (data) => {
-        let id = socket.id;
-        let selectorString = data.selectorString;
-        let iframeIndex = data.iframeIndex;
         socket.broadcast.emit("eventrecord", {
-            id, selectorString, iframeIndex
+            selectorString: data.selectorString, iframeIndex: data.iframeIndex
         })
     });
-
+    // checked
     socket.on("mousemove", (data) => {
-        const x = data.x;
-        const y = data.y;
-        const id = socket.id;
-        const iframeIndex = data.iframeIndex;
         socket.broadcast.emit("locationrecord", {
-            id, x, y, iframeIndex
+            x: data.x, y: data.y, iframeIndex: data.iframeIndex,
+            isLocked: data.isLocked
         })
     })
-
+    //checked
     socket.on("mousewheel", (data) => {
-        const scrollY = data.scrollY;
-        const iframeIndex = data.iframeIndex;
-        const scaledX = data.scaledX;
-        const scaledY = data.scaledY;
-
         socket.broadcast.emit("scrollrecord", {
-            scrollY, scaledX, scaledY, iframeIndex
+            scrollY: data.scrollY, scaledX: data.scaledX, scaledY: data.scaledX,
+            iframeIndex: data.iframeIndex
         })
     })
-
+    //checked
     socket.on("canvasnavigation", (data) => {
-        const iframeIndex = data.iframeIndex;
-        const canvasUrl = data.canvasUrl;
         socket.broadcast.emit("canvasnavigationrecord", {
-            canvasUrl, iframeIndex
+            canvasUrl: data.canvasUrl, iframeIndex: data.iframeIndex
+        })
+    })
+    //checked
+    socket.on("canvasclick", (data) => {
+        socket.broadcast.emit("canvasclickrecord", {
+            iframeIndex: data.iframeIndex,
+            x: data.x,
+            y: data.y
         })
     })
 
     socket.on("setLockState", (data) => {
-        const iframeIndex = data.iframeIndex;
-        const lockState = data.lockState;
         socket.broadcast.emit("setLockStateValue", {
-            lockState, iframeIndex
+            lockState: data.lockState, iframeIndex: data.iframeIndex
         })
-    })
-
-    socket.on("addedmutationrecord", (record) => {
-        const mutationTarget = record.mutationTarget;
-        const addedElemHTML = record.addedElemHTML;
-        const iframeIndex = record.iframeIndex;
-        socket.broadcast.emit("addedmutation", {
-            mutationTarget, addedElemHTML, iframeIndex
-        });
-    });
-
-    socket.on("removedmutationrecord", (record) => {
-        const iframeIndex = record.iframeIndex;
-        socket.broadcast.emit("deletemutations", { iframeIndex });
-
-    });
-
-    socket.on("modifiedAttributeRecord", (record) => {
-        const changedAttribute = record.changedAttribute;
-        const mutationTarget = record.mutationTarget;
-        const mutationValue = record.mutationValue;
-        const mutatedElemHTML = record.mutatedElemHTML;
-        const iframeIndex = record.iframeIndex;
-        socket.broadcast.emit("attributeModify", {
-            changedAttribute, mutationTarget, mutationValue, mutatedElemHTML, iframeIndex
-        });
-
     })
 
     socket.on("disconnect", (reason) => {
